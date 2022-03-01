8012e29480aa35eb11fc5120adff0808960d95ab
becembrahmi@gmail.com <becembrahmi@gmail.com>
2022-03-01 15:00:44 +0100

    first try

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from subprocess import Popen
+import subprocess
 from flask import Flask
 from flask import render_template
 from flask import request
 import os
 app = Flask(__name__)
-
+import re
 
 @app.route("/")
 def hello_world():
@@ -12,5 +14,20 @@ def hello_world():
 
 @app.route('/download', methods=['POST'])
 def download():
-    os.system(f"yt-dlp -x  --audio-format mp3 {request.form['url']}")
-    return render_template('download.html',url=request.form['url'])
+   
+    p = Popen(f"yt-dlp --restrict-filenames  -x --no-playlist -o '%(title)s.%(ext)s' --audio-format mp3 {request.form['url']}", shell=True, stdout=subprocess.PIPE,
+              stderr=subprocess.PIPE, universal_newlines=True)
+
+    output, errors = p.communicate()
+    name=output.split('\n')[-3].replace('[ExtractAudio] Destination: ', '')
+    p = Popen(f'aws s3 cp {name}  s3://becem-youtube-app  --content-disposition=attachment',shell=True)
+    p.wait()
+    print('COMPLETED')
+    f=open('logs.txt','a')
+    f.write("**********************************************************************************************\n")
+    f.write(output)
+    f.write("**********************************************************************************************\n\n\n\n")
+    f.close()
+    #os.system(f"aws s3 cp b.mp3 s3://becem-youtube-app  --content-disposition=attachment")
+    url = "https://becem-youtube-app.s3.eu-west-3.amazonaws.com/"+name
+    return render_template('download.html', url=url)
